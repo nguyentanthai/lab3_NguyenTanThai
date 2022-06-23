4a6cff5a18cff8ef56f417d2db131a5451bba768
nguyentanthai <nguyentanthai2017@gmail.com>
2022-06-23 23:50:23 +0700

    Thêm code

--- lab3_NguyenTanThai/Controllers/AttendancesController.cs
+++ lab3_NguyenTanThai/Controllers/AttendancesController.cs
@@ -1,4 +1,5 @@
-﻿using lab3_NguyenTanThai.Models;
+﻿using lab3_NguyenTanThai.DTOs;
+using lab3_NguyenTanThai.Models;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -19,16 +20,20 @@ namespace lab3_NguyenTanThai.Controllers
             _dbContext = new ApplicationDbContext();
         }
         [HttpPost]
-        public IHttpActionResult Attend([FromBody] int courseId)
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
         {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendance already exists!");
+
             var attendance = new Attendance
             {
-                CourseId = courseId,
-                AttendeeId = User.Identity.GetUserId()
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = userId
             };
             _dbContext.Attendances.Add(attendance);
             _dbContext.SaveChanges();
-            return Ok();      
+            return Ok();
         }
     }
 }
